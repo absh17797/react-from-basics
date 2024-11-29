cc511f9e72133e90f306052a5b22c19264ec1f4f
Absh <abhisheksharma_2@zapbuild.com>
2024-11-29 21:41:48 +0530

    feat: prettier and eslint

---
+++  .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier', // Disables conflicting ESLint rules
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jsx-a11y', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error', // Shows Prettier issues as ESLint errors
+    'react/prop-types': 'off',   // Optional: Disable prop-types validation
+  },
+};
